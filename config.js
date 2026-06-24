2211fc61888dd40727cd746eb14c23a6c25405bd
davidppppp-byte <d1234882323@gmail.com>
2026-06-24 23:41:16 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,3 +5,4 @@
 
 const SUPABASE_URL = 'https://hqtlkdhtdwgrusvmqymm.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhxdGxrZGh0ZHdncnVzdm1xeW1tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0MzQ3MzYsImV4cCI6MjA5NTAxMDczNn0.ilYO3-l2r2HHv0FTSCGdnzH7-nOBZOEcuR-fKj2Lw_0';
+
